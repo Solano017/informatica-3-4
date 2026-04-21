7ebcc2608b3192a395b6104fbccedcd761df82b8
Solano017 <solanoabdiel04@gmail.com>
2026-04-21 15:48:27 +0000

    progress

---
+++ naming-variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int number; // We remove ! because we can´t have any special characters
+int test_value; // We change the space for a _ since spaces are not allowed
+int example1; // Variables always have to start with letters, cant start with a number
+float int_float; // int by itself was a reserved keyword so i changed it into something valid.
+printf("Variables created successfully.\n");
+
+}
