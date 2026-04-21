cc627b2eaed73fb5725b2db0bd8dc2cb3af69661
Solano017 <solanoabdiel04@gmail.com>
2026-04-21 14:49:40 +0000

    variable-names

--- recipe.c
+++ recipe.c
@@ -5,4 +5,15 @@ int main(void)
 {
     printf("Coconut Cranberry Dessert\n");
     printf("\n");
-}
+    printf(" INGREDIENTS:\n");
+    printf("1.  1 1/2 cups \t graham cracker crumbs (about 24 full-sized graham crackers) \n");
+    printf("2.  1/2 cups\t butter or margarine, melted) \n");
+    printf("3.  1 can\t (12 ounces/340 g) sweetened condensed milk \n");
+    printf("4.  1 cup\t flaked coconut  \n");
+    printf("5.  1 cup\t dried cranberries \n");
+    printf("6.  1 cup\t vanilla chips \n");
+    printf("7.  2 cups\t sliced almonds or pecans \n");
+    printf(" STEPS\n");
+    printf("1 \tCombine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan. \n");
+    printf("2 \tCombine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares \n");
+    }
