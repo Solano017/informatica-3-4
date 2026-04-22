4300b1c9da7eab3477a995fe0c2f0c1b8d4a6259
Solano017 <solanoabdiel04@gmail.com>
2026-04-22 15:28:28 +0000

    finished vending-machine

---
+++ vending-machine.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    //Product 1 Chips (fuego)
+    float price1 = 1.5; //usd
+    int quantity1 = 20;
+    char select1 = "A";
+    //Product 2 Coke
+    int price2 = 2
+    int quantity2 = 50 //50 cokes because is the elixir of life.
+    char select2 = "B" // "char" stores one character (letter or number)
+    //Product 3 ChatGPT 1 month
+    float price3 = 5.99; // YES, there was no reason for this comment
+    int quaintity3 = 10;
+    char select3 = "B";
+    printf("Vending Machine \n");
+    printf("Select Item \n");
+}
