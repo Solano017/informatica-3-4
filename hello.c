a36276d529f0b063271031c2fc409657b93fba2f
Solano017 <solanoabdiel04@gmail.com>
2026-04-23 21:05:41 +0000

    ask name

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void)
-
 {
-    printf("hello Abdiel! \n");
+char name[50]; // Variable to store the user's name
+
+printf("Enter your name: "); // Ask the user for their name
+scanf("%s", name); // Save the name entered by the user
+printf("Hello, %s!\n", name); // Print Hello + the user's name
 }
+// End of the program
