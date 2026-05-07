464f1233282dceb7a65771e389e19682bb1a9529
Solano017 <solanoabdiel04@gmail.com>
2026-05-07 20:18:59 +0000

    temp2

---
+++ temperature2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float celsius;
+    float fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f, &celsius");
+    fahrenheit = (celsius * 1.8) + 32;
+    printf("");
+}
